570d0457b673ea41c82e77d2ea30f71a9c1cc3f1
Heather Jackson <hmjackson917@gmail.com>
2021-06-05 12:00:39 -0400

    basic files from mini project

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS resaleStore_db;
+CREATE DATABASE resaleStore_db;
---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Item = require('./Item');
+
+User.hasMany(Item, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Item.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Item };
