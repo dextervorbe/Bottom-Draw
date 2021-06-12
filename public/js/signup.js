a19445893b92dbab99738aca858b4fe79ec3b394
Dexter Vorbe <dextervorbe@ymail.com>
2021-06-12 12:01:07 -0400

    added signup.js, removed lines of login

---
+++ public/js/signup.js
@@ -0,0 +1,29 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    console.log("Im here")
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        console.log(email);
+        console.log(password);
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+console.log("wtf");
+
+document.querySelector('.submitBtn').addEventListener('click', signupFormHandler);
