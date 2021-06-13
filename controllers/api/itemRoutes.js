95e200220f2b6bc47108526901f15daabd391664
Amy Smerlick <77814900+amysmerlick@users.noreply.github.com>
2021-06-13 11:57:20 -0400

    updated Dashboard, added images for items, updated delete function

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -2,17 +2,20 @@ const router = require('express').Router();
 const { Item } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+//,,
 router.post('/', withAuth, async (req, res) => {
+
   try {
     const newItem = await Item.create({
       ...req.body,
-      user_id: req.session.user_id,
+      user_id: req.session.user_id
     });
 
-    res.status(200).json(newItem);
+    res.redirect("/dashboard")
   } catch (err) {
     res.status(400).json(err);
   }
+ 
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
